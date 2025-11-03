ae91b851cd949dc5b7a7668a72eda8772a59615d
GiovanniArwen <jovanierwen656@gmail.com>
2025-11-03 12:54:04 +0200

    remove Details text

--- lib/features/home/presentation/widgets/upper_card.dart
+++ lib/features/home/presentation/widgets/upper_card.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rash7ly/core/utilis/app_colors.dart';
-import 'package:rash7ly/core/utilis/text_style.dart';
 import 'package:rash7ly/features/home/presentation/widgets/_build_icon.dart';
 
 class UpperCard extends StatelessWidget {
@@ -21,13 +19,7 @@ class UpperCard extends StatelessWidget {
               icon: buildicon(icon: Icons.arrow_back_ios_new),
               // icon: _build_icon(Icons.arrow_back_ios_new_rounded),
             ),
-            Text(
-              'Details',
-              style: TextStyles.getSize18(
-                fontWeight: FontWeight.bold,
-                color: AppColors.whiteColor,
-              ),
-            ),
+
             IconButton(
               onPressed: () {},
               icon: buildicon(icon: Icons.bookmark_border),
